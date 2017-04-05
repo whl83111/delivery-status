88574b39620177cc8c3e1d3086daecf834f6637c
Tommy Lai <tommy1994111@gmail.com>
2017-04-05 18:02:44 +0800

    deploy on gcp

--- main.py
+++ main.py
@@ -36,4 +36,4 @@ def getData():
 		return jsonify(deliveryId = deliveryId, DongPoo = DongPoo, BlackCat = BlackCat)
 
 if __name__ == "__main__":
-	app.run(host = '0.0.0.0', debug = False)
+	app.run(host = '127.0.0.1', port = '5000', debug = False)
